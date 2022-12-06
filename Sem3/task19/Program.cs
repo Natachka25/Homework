ff790b6eea767cee785cbd988d2c73d5265aa188
Nata <nata25.95@yandex.ru>
2022-12-07 00:49:12 +0300

    Sem 3_1

--- Sem3/task19/Program.cs
+++ Sem3/task19/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите пятизначное число:");
+﻿/*
+Console.WriteLine("Введите пятизначное число:");
 string array = Console.ReadLine()!;
 
 int i = 0;
@@ -12,4 +13,33 @@ if (array[i] == array[4 + i] && array[1 + i] == array[3 + i])
 else
 {
     Console.WriteLine("Число не является палиндроном");
+}
+*/
+
+Console.WriteLine("Введите число:");
+string array = Console.ReadLine()!;
+
+int size = array.Length;
+int i = 0;
+
+if (array[0] == '-') i = 1;
+
+bool result = true;
+while (i < size/2)
+{
+    if (array[i] != array[size - 1 - i]) 
+    {
+        result = false;
+        break;
+    }
+    i++;
+}
+
+if (result == true) 
+{
+    System.Console.WriteLine("Палиндром");
+} 
+else 
+{
+    System.Console.WriteLine("Не палиндром");
 }
